03585e37dd92f27489729d1a307072bf05a07ffc
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-08-23 21:57:16 +0300

    task imageSearch frontend

--- src/index.js
+++ src/index.js
@@ -1,91 +1,104 @@
-import { fetchBreeds } from './cat-api.js';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getPhotosByWord } from './pixabayAPI';
 
 const refs = {
-  breedSelect: document.querySelector('.breed-select'),
-  catInfo: document.querySelector('.cat-info'),
-  loaderElem: document.querySelector('.loader'),
-  errorElem: document.querySelector('.error'),
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  target: document.querySelector('.target-load'),
 };
 
-function renderSelect() {
-  setTimeout(() => {
-    fetchBreeds()
-      .then(data =>
-        data.forEach(({ id, name }) => {
-          const markup = `<option value="${id}">${name}</option>`;
-          refs.breedSelect.insertAdjacentHTML('beforeend', markup);
-        })
-      )
-      .catch(error => {
-        return Notify.failure(
-          'Oops! Something went wrong! Try reloading the page!',
-          {
-            position: 'center-center',
-            clickToClose: true,
-            fontSize: '26px',
-            width: 'fit-content',
-          }
-        );
-      })
-      .finally(() => {
-        new SlimSelect({
-          select: refs.breedSelect,
-        });
-        loader();
-        Notify.info('Please choose a cat breed', {
-          position: 'center-center',
-          clickToClose: true,
-          fontSize: '26px',
-          width: 'fit-content',
-        });
-      });
-  }, 1000);
+let maxPage = 1;
+let currentPage = 1;
+
+refs.form.addEventListener('submit', onSearchSubmit);
+
+function onSearchSubmit(e) {
+  e.preventDefault();
+  refs.gallery.innerHTML = '';
+
+  getPhotosByWord(e.target.elements.searchQuery.value)
+    .then(({ res, page }) => {
+      currentPage = page;
+      maxPage = Math.ceil(res.totalHits / res.hits.length);
+
+      renderMarkup(res.hits);
+      observer.observe(refs.target);
+      updateStatusObserver();
+      lightbox.refresh();
+    })
+    .catch(err => {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      console.log(err);
+    });
+  e.target.reset();
 }
-renderSelect();
+const observer = new IntersectionObserver(onLoad);
 
-refs.breedSelect.addEventListener('change', onSelectBreed);
-
-function onSelectBreed(e) {
-  loader();
-  setTimeout(() => {
-    fetchBreeds()
-      .then(res => {
-        const filteredCat = {
-          ...res.filter(el => e.target.value === el.id)[0],
-        };
-        const { image, name, description, temperament } = filteredCat;
-
-        const markup = `<img class="image" src="${image.url}" alt="${name}">
-        <div class="text-box">
-        <h2 class="desc-title">${name}</h2>
-        <p class="desc-text">${description}</p>
-        <p><b>Temperament</b>${temperament}</p>
-        </div>`;
-
-        refs.catInfo.innerHTML = markup;
-      })
-      .catch(error => {
-        return Notify.failure(
-          'Oops! Something went wrong! Try reloading the page!',
-          {
-            position: 'center-center',
-            clickToClose: true,
-            fontSize: '26px',
-            width: 'fit-content',
-          }
-        );
-      })
-      .finally(() => {
-        loader();
+function loadMore() {
+  getPhotosByWord()
+    .then(({ res, page }) => {
+      console.log(page);
+      currentPage = page;
+      renderMarkup(res.hits);
+      window.scrollBy({
+        top: 690,
+        behavior: 'smooth',
       });
-  }, 600);
+      updateStatusObserver();
+      lightbox.refresh();
+    })
+    .catch(err => console.log(err));
 }
 
-function loader() {
-  refs.catInfo.classList.toggle('visually-hidden');
-  refs.breedSelect.classList.toggle('visually-hidden');
-  refs.loaderElem.classList.toggle('visually-hidden');
+function onLoad(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      loadMore();
+    }
+  });
+}
+
+function renderMarkup(hits) {
+  const markup = hits.map(markupTemplate).join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function markupTemplate({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `<div class="photo-card">
+    <a href="${largeImageURL}"><img class="image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+    <div class="info">
+    <p class="info-item">
+      <b>${likes} &#128077;</b>
+    </p>
+    <p class="info-item">
+      <b>${views} &#128064;</b>
+    </p>
+    <p class="info-item">
+      <b>${comments} &#128221;</b>
+    </p>
+    <p class="info-item">
+      <b>${downloads} &#128228;</b>
+    </p>
+  </div>
+</div>`;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {});
+
+function updateStatusObserver() {
+  if (currentPage === maxPage) {
+    observer.unobserve(refs.target);
+  }
 }
